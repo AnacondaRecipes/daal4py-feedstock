95d5e4f2c6e5bd88d9982a56c770be91ccb71e84
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-18 12:36:54 +0300

    Fix a test command

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -12,7 +12,7 @@ fi
 
 # Upstream tests
 ${PYTHON} -c "import daal4py"
-mpirun -n 4 ${PYTHON} -m unittest discover -v -p spmd*.py
+mpiexec -n 4 ${PYTHON} -m unittest discover -v -p spmd*.py
 ${PYTHON} -m unittest discover -v -p 'test*[!ex].py'
 pytest --verbose --pyargs daal4py/sklearn/
 #pytest --verbose --pyargs onedal/ --deselect="onedal/common/tests/test_policy.py" --deselect="onedal/svm/tests/test_svc.py::test_estimator"
